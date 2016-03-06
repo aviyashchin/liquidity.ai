ebebd3e60c66803670a634c4120f9f9755fe1a0a
Avi Yashchin <avi.yashchin@gmail.com>
2016-03-06 00:29:14 -0500

    adding data .sh

--- data/getLoan_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#python fx_lstm1.py -i data/clean_tick_data_1_min_step.csv -o output.csv
-wget https://s3.amazonaws.com/liquidity.ai/Loan-Data/loan-data.zip
-unzip loan-data.zip
-rm loan-data.zip
