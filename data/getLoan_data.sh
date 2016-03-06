e866466300dd54a448c7d648f6fc673558779b12
avi yashchin <avi.yashchin@gmail.com>
2016-03-06 04:54:45 +0000

    cleaning

--- data/getLoan_data.sh
+++ data/getLoan_data.sh
@@ -2,3 +2,4 @@
 #python fx_lstm1.py -i data/clean_tick_data_1_min_step.csv -o output.csv
 wget https://s3.amazonaws.com/liquidity.ai/Loan-Data/loan-data.zip
 unzip loan-data.zip
+rm loan-data.zip
--- runlstm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python fx_lstm1.py -i data/clean_tick_data_1_min_step.csv -o output.csv
